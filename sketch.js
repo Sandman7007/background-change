9367343399d637a94786578aa9f4f630dcbf084a
Sandman7007 <91296564+Sandman7007@users.noreply.github.com>
2021-09-27 19:14:35 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+var weights=[345,700,300,200,500]
+function avg(){
+  var sum=weights[0]+weights[1]+weights[2]+weights[3]+weights[4]
+  var average = sum/sum.length
+  console.log(average) 
+}
+
+avg()
+
+
+
+
